de33b0d42fb9e7da59da5c9dad8b802146edb806
praveen3073 <pop3073@gmail.com>
2020-09-24 19:09:35 +0530

    Added: Start Position

--- snakenladder.java
+++ snakenladder.java
@@ -2,5 +2,7 @@
 public class snakenladder {
 	public static void main(String args[]){
 		System.out.println("Welcome to Snake & Ladder Simulator");
+		final int START_POSITION = 0;
+		int position = START_POSITION;
 	}
 }
