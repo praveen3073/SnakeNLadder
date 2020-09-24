1bf9487561b2b7d598b1a9983def1c0f8eda1dd8
praveen3073 <pop3073@gmail.com>
2020-09-24 19:16:08 +0530

    Added: UC2 Random Die Number

--- snakenladder.java
+++ snakenladder.java
@@ -4,5 +4,7 @@ public class snakenladder {
 		System.out.println("Welcome to Snake & Ladder Simulator");
 		final int START_POSITION = 0;
 		int position = START_POSITION;
+		int dieNumber = (int) Math.floor(Math.random()*60)%6 + 1;
+		System.out.println("The number on the die is: " + dieNumber);
 	}
 }
